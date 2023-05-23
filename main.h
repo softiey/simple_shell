3547f04b940f13321a5af3a9e67f3be91814c48e
softiey <im_adam@yahoo.com>
2023-05-22 23:40:07 -0700

    pushed from sandbox

--- main.h
+++ main.h
@@ -12,4 +12,80 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#endif
+/* buffers */
+#define RBSIZE 1024
+#define WBSIZE 1024
+#define BFLUSH -1
+
+extern char **environ;
+/**
+ * struct liststr using singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: always points to the next node
+ */
+typedef struct l_str
+{
+	int num;
+	char *str;
+	struct l_str *next;
+} l_str_t;
+
+/**
+ *struct passinginfo - contains pseudo-arguements to pass into a function,
+ *		allowing uniform prototype for function pointer struct
+ *@arg: a string generated from getline containing arguements
+ *@argv: an array of strings generated from arg
+ *@path: a string path for the current command
+ *@argc: the argument count
+ *@line_count: the error count
+ *@err_num: the error code for exit()s
+ *@linecount_flag: if on count this line of input
+ *@fname: the program filename
+ *@env: linked list local copy of environ
+ *@environ: custom modified copy of environ from LL env
+ *@history: the history node
+ *@alias: the alias node
+ *@env_changed: on if environ was changed
+ *@status: the return status of the last exec'd command
+ *@cmd_buf: address of pointer to cmd_buf, on if chaining
+ *@cmd_buf_type: CMD_type ||, &&, ;
+ *@readfd: the fd from which to read line input
+ *@histcount: the history line number count
+ */
+typedef struct passinginfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	l_str_t *env;
+	l_str_t *history;
+	l_str_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} p_info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(p_info_t *);
+} builtin_table;
